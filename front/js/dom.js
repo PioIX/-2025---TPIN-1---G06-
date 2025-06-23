43c965d971c0b7a20b3b5c165f295a6b75bac072
JuaniTeruya <jteruya@pioix.edu.ar>
2025-06-23 09:39:43 -0300

    Se soluciona merge conflict
    * Hay que corregir ingresar en HTML

--- front/js/dom.js
+++ front/js/dom.js
@@ -8,40 +8,21 @@ class UserInterface {
     getPassword() {
         return document.getElementById("password").value;
     }
-
     getNombre() {
         return document.getElementById("nombre").value;
     }
 
-    getSelectBorrar() {
-        return document.getElementById("nombre").value;
-    }
-    
-    
-    getJugadoresMod() {
-        return document.getElementById("ingresoJugadoresMod").value
-    }    
-    
-    getNombreCr() {
-        return document.getElementById("nombreAgregar").value
-    }    
-    
-    getImgCr() {
-        return document.getElementById("imagenAgregar").value
-    }    
-    
-    getGolesCr() {
-        return document.getElementById("golesAgregar").value
-    }    
-    
-    
-
-
 
     showModal(title, body) {
-        document.getElementById("modalTitle").textContent = title;
-        document.getElementById("modalBody").textContent = body;
-        document.getElementById("modal").style.display = "block";
+    document.getElementById("modalTitle").textContent = title;
+    document.getElementById("modalBody").textContent = body;
+    document.getElementById("modal").style.display = "block";
     }
+
+    closeModal() {
+        document.getElementById("modal").style.display = "none";
+    }
+
+    
 }
 const ui = new UserInterface();
